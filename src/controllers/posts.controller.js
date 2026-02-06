1d25caadcc0ee467271553ce816fba3359251f2b
Akhilan Theiventhiran <Akhilan@MacBookPro.mshome.net>
2026-02-06 14:34:46 +0530

    Initial commit

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -5,14 +5,14 @@ const getAllPosts = (req, res) => {
   // For now, the "business logic" is simple.
   // In the future, this is where we would call a service to get data from a database.
   const posts = [
-    { id: 1, title: 'Controller Post 1' },
-    { id: 2, title: 'Controller Post 2' }
+    { id: 1, title: "Controller Post 1" },
+    { id: 2, title: "Controller Post 2" },
   ];
 
   // The controller's job is to send the final response.
   res.status(200).json({
-    message: 'Route handled by postController.getAllPosts',
-    data: posts
+    message: "Route handled by postController.getAllPosts",
+    data: posts,
   });
 };
 
