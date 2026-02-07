759cd39184450ff5864f4120070ab4472da631e6
Akhilan Theiventhiran <Akhilan@Akhilans-MacBook-Pro.local>
2026-02-07 10:36:20 +0530

    feat: add endpoint to get post by id

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,22 +1,30 @@
 // src/controllers/posts.controller.js
 
-// This function will be our handler for "GET /api/v1/posts"
 const getAllPosts = (req, res) => {
-  // For now, the "business logic" is simple.
-  // In the future, this is where we would call a service to get data from a database.
   const posts = [
     { id: 1, title: "Controller Post 1" },
     { id: 2, title: "Controller Post 2" },
   ];
 
-  // The controller's job is to send the final response.
   res.status(200).json({
     message: "Route handled by postController.getAllPosts",
     data: posts,
   });
 };
 
-// We export the function in an object so we can easily add more functions later.
+// 1. Create the new async function
+const getPostById = async (req, res) => {
+  // 2. Retrieve the post ID from req.params.postId
+  const postId = req.params.postId;
+
+  // 3. Send the JSON response
+  res.status(200).json({
+    message: "Fetching data for post with ID: " + postId
+  });
+};
+
+// 4. Export the new function
 module.exports = {
   getAllPosts,
+  getPostById,
 };
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,14 +3,12 @@
 const express = require("express");
 const router = express.Router();
 
-// 1. Import the controller
 const postController = require("../controllers/posts.controller.js");
 
-// 2. Use the controller function as the route handler
-// The router's job is now just to connect the path '/' to the 'getAllPosts' function.
+// Order is important: static routes first
 router.get("/", postController.getAllPosts);
 
-// We can remove the old inline function entirely!
-// router.get('/', (req, res) => { ... }); // This is now gone
+// 5. Add the new route with the :postId parameter
+router.get('/:postId', postController.getPostById);
 
 module.exports = router;
