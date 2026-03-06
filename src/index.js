051c3d4a253060d7af55edfd1b652a2160c1bfb0
Akhilan Theiventhiran <Akhilan@Akhilans-MacBook-Pro.local>
2026-03-06 10:21:10 +0530

    Mongoose install

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-
 // 1. Import our new post router
 const postRouter = require("./routes/posts.routes.js");
 
 // 2. Mount the router (ADD THIS LINE)
 // This tells Express to send any request starting with /api/v1/posts
 // to the postRouter.
-const mainRouter = require('./routes');
-app.use('/api/v1', mainRouter);
+const mainRouter = require("./routes");
+app.use("/api/v1", mainRouter);
 
 app.get("/welcome", (req, res) => {
   res.send("Welcome to the Blogify API!");
